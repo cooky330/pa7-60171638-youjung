7dda60ca1a08e325f1d948244a4c6fc32d5745c0
soyoujung <cooky330@naver.com>
2017-11-19 21:59:55 +0900

    pa7 과제 commit

--- src/ShapeTest.java
+++ src/ShapeTest.java
@@ -18,4 +18,11 @@ public class ShapeTest {
 			d.draw();
 		}
 	}
-}                                               
+}   
+/*주석-60171638 소유정:
+먼저 Drawable을 interface로 작성하고 draw라는 메소드를 하나 생성한다.
+Shape의 interface가 Drawable이므로 Shape는 추상클래스여야 한다. 
+Shape안에 좌표를 받는 인자와, 넓이와 둘레를 각각 받는 메소드를 생성한다.
+이제 Shape의 하위클래스인 Rectangle과 Circle클래스안의 넓이와 둘레 메소드를 각각 override해준다.
+draw메소드까지 override해준다. Drawable을 interface로 받는 Text 클래스도 만들어 draw메소드를 override한다. 
+*/
